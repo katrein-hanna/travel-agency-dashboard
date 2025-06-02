b2327366d3a6a394625096ff76f07db0d23b5c15
Katrein Hanna <katrein.hanna.96@gmail.com>
2025-06-02 13:41:56 +0300

    Implement Routing & Dashboard Layout

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,8 +1,16 @@
+import { Header } from 'components'
 import React from 'react'
 
 function AllUsers() {
   return (
-    <div>Users Table</div>
+    <main className='dashboard wrapper'>
+      <Header 
+      title="Trips page"
+      description="Check out current users in real time"
+      />
+
+      All Users Page Contents
+    </main>
   )
 }
 
